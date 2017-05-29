80b64f42dc4fee112bc8fdf204c11a61e691b83a
Ivonne Terrero <iqcodes21@gmail.com>
2017-05-28 22:07:31 -0700

    switching branches because communication with firebase is faulty.

--- src/actions/skills.js
+++ src/actions/skills.js
@@ -5,6 +5,12 @@ export const updateSkillsAndSendToDB = (userData, skill) => {
   addSkillToFireBase(userName, skill);
   return skill;
 };
+//pertaing to Update skill
+export const updateSkillOnFireBase = (userName, skill, skillId) => {
+  const skillInFB = firebase.database().ref(`users/${userName}/skills/${skillId}`);
+  skillInFB.update(skill);
+  return skill;
+};
 
 export const addSkillToFireBase = (userName, skill) => {
   const userSkillsInFireBase = firebase.database().ref(`users/${userName}/skills`).push();
@@ -15,4 +21,9 @@ export const addSkillToFireBase = (userName, skill) => {
 export const addSkill = (userData, skill) => ({
   type: 'ADD_SKILL',
   payload: updateSkillsAndSendToDB(userData, skill),
+});
+//pertaining to update Skill
+export const updataSkill = (userData, skill) => ({
+  type: 'UPDATE_SKILL',
+  payload: updateSkillOnFireBase(userData, skill),
 });
